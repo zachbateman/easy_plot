f6092b81d8db71b782313014830000841be4dabb
Zach Bateman <zkbateman@gmail.com>
2019-03-09 12:43:14 -0600

    Modify scatterplot_matrix_plot to use kde on diagonals and add linear regression; also add title to plot

--- statplot/scatterplot_matrix_plot.py
+++ statplot/scatterplot_matrix_plot.py
@@ -6,9 +6,10 @@ import seaborn as sns
 
 
 
-def scatterplot_matrix_plot(df) -> None:
+def scatterplot_matrix_plot(df, title='Pairplot Title') -> None:
     '''Display a scatterplot matrix for the df arg'''
-    sns.pairplot(df)
+    g = sns.pairplot(df, kind='reg', diag_kind='kde')
+    g.fig.suptitle(title, y=0.96)
 
     plt.subplots_adjust(left=0.08, right=0.95, bottom=0.08, top=0.90, wspace=0.10)
     plt.show()
