fe1a49d1406bbac19ec08f432bf472b55ccbd17e
Zach Bateman <zkbateman@gmail.com>
2019-03-12 17:19:39 -0500

    Formatting pca.py

--- statplot/pca.py
+++ statplot/pca.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 
 
 def pca_screeplot(df):
-    
+
     standardized_df = statplot.cleaning.standardize_df(df)
     pca = sklearn.decomposition.PCA().fit(standardized_df)
-    
+
     y = numpy.std(pca.transform(standardized_df), axis=0) ** 2
     x = numpy.arange(len(y)) + 1
-    
+
     plt.plot(x, y, 'o-')
     plt.xticks(x, df.columns, rotation=60)
     plt.ylabel('Variance')
