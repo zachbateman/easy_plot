e73ea662a93979ce885c3dc79b19783487dd4cfd
Zach Bateman <zkbateman@gmail.com>
2019-04-16 13:20:01 -0500

    Add kwarg to show major and/or minor gridlines in scatterplot

--- statplot/scatterplot.py
+++ statplot/scatterplot.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 
 
-def scatterplot(df, xvar: str='', yvar: str='', sizevar: str='', colorvar: str='', stylevar: str='', size_minmax: tuple=(50  , 300), alpha: float=0.9, title='Scatterplot', largest_fontsize: int=17, xlog=False, ylog=False) -> None:
+def scatterplot(df, xvar: str='', yvar: str='', sizevar: str='', colorvar: str='', stylevar: str='', size_minmax: tuple=(50  , 300), alpha: float=0.9, title='Scatterplot', largest_fontsize: int=17, xlog=False, ylog=False, major_gridlines=False, minor_gridlines=False) -> None:
     '''
     Display a (very nicely formatted) scatter plot for the df arg.
     Can visualize up to 5 variables by optionally using
@@ -24,6 +24,12 @@ def scatterplot(df, xvar: str='', yvar: str='', sizevar: str='', colorvar: str='
         ax.set_xscale('log')
     if ylog:
         ax.set_yscale('log')
+
+    if major_gridlines:
+        ax.grid(which='major', linestyle='-', linewidth=1, color='#bbbbbb')
+    if minor_gridlines:
+        ax.minorticks_on()
+        ax.grid(which='minor', linestyle='-', linewidth=0.5, color='#cccccc')
 
     plt.subplots_adjust(left=0.08, right=0.95, bottom=0.08, top=0.90, wspace=0.10)
     plt.show()
