b054cb513eb94ed6475b258b6a9d1a922724b7b7
Zach Bateman <zkbateman@gmail.com>
2019-04-24 00:12:02 -0500

    Modify scatterplot matrix to use higher-order regression on scatter plots; also change bottom-left plots to kde plots; update example plot image

--- statplot/scatterplot_matrix_plot.py
+++ statplot/scatterplot_matrix_plot.py
@@ -6,10 +6,15 @@ import seaborn as sns
 
 
 
-def scatterplot_matrix_plot(df, title='Scatterplot Matrix') -> None:
+def scatterplot_matrix_plot(df, title='Scatterplot Matrix', regression_order=2) -> None:
     '''Display a scatterplot matrix for the df arg'''
-    g = sns.pairplot(df, kind='reg', diag_kind='kde')
-    g.fig.suptitle(title, y=0.96)
 
+    g = sns.PairGrid(df, diag_sharey=False)
+    g.map_lower(sns.kdeplot)
+    g.map_upper(sns.regplot, order=regression_order)
+    g.map_diag(sns.kdeplot, lw=3, shade=True)
+
+
+    g.fig.suptitle(title, y=0.96)
     plt.subplots_adjust(left=0.08, right=0.95, bottom=0.08, top=0.90, wspace=0.10)
     plt.show()
